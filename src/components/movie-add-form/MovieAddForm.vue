ff4a34b8a99e3721ad315f223c82510f0bcdffdc
esonaliyev05 <esonaliyevalyorbek911@gmail.com>
2025-08-13 14:12:00 +0500

    new change

--- src/components/movie-add-form/MovieAddForm.vue
+++ src/components/movie-add-form/MovieAddForm.vue
@@ -1,6 +1,14 @@
 <template >
-    <div>
-        <h1>Movie-add-form</h1>
+    <div class="movie-add-form">
+        <h3>Yangi kino qoshish</h3>
+
+        <form class="add-form d-flex">
+            <input type="text" class="form-control new-label" placeholder="qanday kino">
+            <input type="text" class="form-control new-label" placeholder="Nechi Marotaba ko'rilgan">
+
+
+            <button class="btn btn-outline-dark">qo'shish</button>
+        </form>
     </div>
 </template>
 <script>
@@ -8,6 +16,14 @@ export default {
     
 }
 </script>
-<style scoped>
+<style >
     
+    .movie-add-form{
+        margin-top: 2rem;
+  margin-bottom: 1rem;
+  padding: 1.5rem;
+  background-color: #fcfaf5;
+  border-radius: 4px;
+  box-shadow: 15px 15px 15px rgb(0, 0, 0, 0.15);
+    }
 </style>
